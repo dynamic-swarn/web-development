7a510f16c02b45dc15e059aebdfd63e94af7ec1a
Swarnamayee Panda <swarnamayee9692@gmail.com>
2026-02-11 04:41:44 +0530

    Add files via upload

---
+++ classes.js
@@ -0,0 +1,12 @@
+class A{
+    #name = "Chora";
+    name = "Mara";
+    show(){
+        return this.name;
+    }
+}
+class B extends A{ 
+    show(){
+        return "Amara aji clg chutti";
+    }
+}
